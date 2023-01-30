852ef2fac87d29a70a552ac562f94e3ef38e3ee3
ivansukhoparov <rbcdaaa@gmail.com>
2023-01-30 21:23:19 +0400

    fix timer 15ms delay

---
+++ source/js/timer.js
@@ -0,0 +1,50 @@
+const startStopButton = document.querySelector('#start-stop-button');
+const timer = document.querySelector('#timer');
+
+let counter = 0;
+let start = false;
+let startTime, currentTime,stopTime;
+
+
+const sleep = function (ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const addZero = function (value) {
+    if (value < 10) {
+        return '0' + value;
+    }
+    return value;
+}
+
+
+
+const printTime = function (value) {
+    const seconds = value % 60; // get seconds
+    const minutes = Math.floor(value / 60 % 60); // get minutes
+    const hour = Math.floor(value / 60 / 60 % 60); // get hours
+    return addZero(hour) + ':' + addZero(minutes) + ':' + addZero(seconds);
+}
+
+const goTimer = async function () {
+    let timerValue;
+    while (start) {
+        currentTime = new Date();
+        counter=Math.round((currentTime-startTime)/1000)
+        timerValue = printTime(counter);
+        timer.textContent = timerValue;
+        await sleep(1000);
+    }
+}
+
+startStopButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    startTime = new Date();
+    if (!start) {
+        start = true;
+        goTimer();
+    } else {
+        stopTime = new Date();
+        start = false;
+    }
+})

985cc8c28ca5008e00e9972ac183f12e35597ec1
ivansukhoparov <rbcdaaa@gmail.com>
2023-02-03 00:33:08 +0400

    realised counting time for start/pause button and resetting counter by stop button

--- source/js/timer.js
+++ source/js/timer.js
@@ -3,14 +3,16 @@ const stopButton = document.querySelector('#stop-button');
 const timer = document.querySelector('#timer');
 
 let counter = 0;
+let sessionTime = 0;
 let start = false;
-let startTime, currentTime, stopTime;
+let startTime, currentTime, pauseTime;
 
-
+// function creating a delay for the specified number of milliseconds
 const sleep = function (ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// function added '0' before 0-9 digits 
 const addZero = function (value) {
     if (value < 10) {
         return '0' + value;
@@ -18,6 +20,7 @@ const addZero = function (value) {
     return value;
 }
 
+// function transforming milliseconds interval to HH:MM:SS format
 const printTime = function (value) {
     const seconds = value % 60; // get seconds
     const minutes = Math.floor(value / 60 % 60); // get minutes
@@ -25,26 +28,37 @@ const printTime = function (value) {
     return addZero(hour) + ':' + addZero(minutes) + ':' + addZero(seconds);
 }
 
-const goTimer = async function () {
+
+const countingTime = async function () {
     let timerValue;
     while (start) {
         currentTime = new Date();
         counter = Math.round((currentTime - startTime) / 1000)
-        timerValue = printTime(counter);
-        timer.textContent = timerValue;
+        timerValue = sessionTime + counter;
+        timer.textContent = printTime(timerValue);
         await sleep(1000);
-    }
+    };
+    sessionTime=timerValue;
 }
 
+
+// add events to START button
 startButton.addEventListener('click', (evt) => {
     evt.preventDefault();
     startTime = new Date();
     if (!start) {
         start = true;
-        goTimer();
+        countingTime();
+        startButton.textContent = 'PAUSE'
     } else {
-        stopTime = new Date();
         start = false;
+        startButton.textContent = 'START'
     }
 })
 
+// add events to STOP button
+stopButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    sessionTime=0;
+    timer.textContent = printTime(0);
+})
